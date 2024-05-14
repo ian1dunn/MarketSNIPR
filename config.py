3cadb20122515cb718b279df551693d66f0dfcc7
Ian Dunn <itd3516@rit.edu>
2024-05-13 21:22:15 -0400

    Move main to root and add config variables

--- config.py
+++ config.py
@@ -1,1 +1,21 @@
 LOOK_BACK_WINDOW = 14  # 14 or 9, how many days to look back to determine past asset prices
+
+#
+# Enviroment & Model Config
+#
+ENV = "HalfCheetah-v2"  # OpenAI gym environment name
+SEED = 0  # Sets Gym, PyTorch and Numpy seeds
+START_TIMESTEPS = 25e3  # Time steps initial random policy is used
+EVAL_FREQ = 5e3  # How often (time steps) we evaluate
+MAX_TIMESTEPS = 1e6  # Max time steps to run environment
+
+EXPL_NOISE = 0.1  # Std of Gaussian exploration noise
+BATCH_SIZE = 256  # Batch size for both actor and critic
+DISCOUNT = 0.99  # Discount factor
+TAU = 0.005  # Target network update rate
+POLICY_NOISE = 0.2  # Noise added to target policy during critic update
+NOISE_CLIP = 0.5  # Range to clip target policy noise
+POLICY_FREQ = 2  # Frequency of delayed policy updates
+
+SAVE_MODEL = True  # Save model and optimizer parameters  (action=store_true)
+LOAD_MODEL = "./model..."  # Model load file name, "" doesn't load, "default" uses file_name
--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import gym
 import argparse
 import os
 
-import utils
-import TD3
+import config
+from model import TD3, utils
 
 
 # Runs policy for X episodes and returns average reward
@@ -31,78 +31,61 @@ def eval_policy(policy, env_name, seed, eval_episodes=10):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--env", default="HalfCheetah-v2")  # OpenAI gym environment name
-    parser.add_argument("--seed", default=0, type=int)  # Sets Gym, PyTorch and Numpy seeds
-    parser.add_argument("--start_timesteps", default=25e3, type=int)  # Time steps initial random policy is used
-    parser.add_argument("--eval_freq", default=5e3, type=int)  # How often (time steps) we evaluate
-    parser.add_argument("--max_timesteps", default=1e6, type=int)  # Max time steps to run environment
-    parser.add_argument("--expl_noise", default=0.1, type=float)  # Std of Gaussian exploration noise
-    parser.add_argument("--batch_size", default=256, type=int)  # Batch size for both actor and critic
-    parser.add_argument("--discount", default=0.99, type=float)  # Discount factor
-    parser.add_argument("--tau", default=0.005, type=float)  # Target network update rate
-    parser.add_argument("--policy_noise", default=0.2)  # Noise added to target policy during critic update
-    parser.add_argument("--noise_clip", default=0.5)  # Range to clip target policy noise
-    parser.add_argument("--policy_freq", default=2, type=int)  # Frequency of delayed policy updates
-    parser.add_argument("--save_model", action="store_true")  # Save model and optimizer parameters
-    parser.add_argument("--load_model", default="")  # Model load file name, "" doesn't load, "default" uses file_name
-    args = parser.parse_args()
-
-    file_name = f"TD3_{args.env}_{args.seed}"
+    file_name = f"TD3_{config.ENV}_{config.SEED}"
     print("---------------------------------------")
-    print(f"Policy: TD3, Env: {args.env}, Seed: {args.seed}")
+    print(f"Policy: TD3, Env: {config.ENV}, Seed: {config.SEED}")
     print("---------------------------------------")
 
     if not os.path.exists("./results"):
         os.makedirs("./results")
 
-    if args.save_model and not os.path.exists("./models"):
+    if config.SAVE_MODEL and not os.path.exists("./models"):
         os.makedirs("./models")
 
-    env = gym.make(args.env)
+    env = gym.make(config.ENV)
 
     # Set seeds
-    env.seed(args.seed)
-    env.action_space.seed(args.seed)
-    torch.manual_seed(args.seed)
-    np.random.seed(args.seed)
+    env.seed(config.SEED)
+    env.action_space.seed(config.SEED)
+    torch.manual_seed(config.SEED)
+    np.random.seed(config.SEED)
 
     state_dim = env.observation_space.shape[0]
     action_dim = env.action_space.shape[0]
     max_action = float(env.action_space.high[0])
 
-    kwargs = {"state_dim": state_dim, "action_dim": action_dim, "max_action": max_action, "discount": args.discount,
-              "tau": args.tau, "policy_noise": args.policy_noise * max_action,
-              "noise_clip": args.noise_clip * max_action, "policy_freq": args.policy_freq}
+    kwargs = {"state_dim": state_dim, "action_dim": action_dim, "max_action": max_action, "discount": config.DISCOUNT,
+              "tau": config.TAU, "policy_noise": config.POLICY_NOISE * max_action,
+              "noise_clip": config.NOISE_CLIP * max_action, "policy_freq": config.POLICY_FREQ}
 
     # Target policy smoothing is scaled wrt the action scale
     policy = TD3.TD3(**kwargs)
 
-    if args.load_model != "":
-        policy_file = file_name if args.load_model == "default" else args.load_model
+    if config.LOAD_MODEL != "":
+        policy_file = file_name if config.LOAD_MODEL == "default" else config.LOAD_MODEL
         policy.load(f"./models/{policy_file}")
 
     replay_buffer = utils.ReplayBuffer(state_dim, action_dim)
 
     # Evaluate untrained policy
-    evaluations = [eval_policy(policy, args.env, args.seed)]
+    evaluations = [eval_policy(policy, config.ENV, config.SEED)]
 
     state, done = env.reset(), False
     episode_reward = 0
     episode_timesteps = 0
     episode_num = 0
 
-    for t in range(int(args.max_timesteps)):
+    for t in range(int(config.MAX_TIMESTEPS)):  # TODO remove config, use based on market data length
 
         episode_timesteps += 1
 
         # Select action randomly or according to policy
-        if t < args.start_timesteps:
+        if t < config.START_TIMESTEPS:  # TODO adjust this to accurately reflect market data length
             action = env.action_space.sample()
         else:
             action = (
                     policy.select_action(np.array(state))
-                    + np.random.normal(0, max_action * args.expl_noise, size=action_dim)
+                    + np.random.normal(0, max_action * config.EXPL_NOISE, size=action_dim)
             ).clip(-max_action, max_action)
 
         # Perform action
@@ -116,8 +99,8 @@ if __name__ == "__main__":
         episode_reward += reward
 
         # Train agent after collecting sufficient data
-        if t >= args.start_timesteps:
-            policy.train(replay_buffer, args.batch_size)
+        if t >= config.START_TIMESTEPS:
+            policy.train(replay_buffer, config.BATCH_SIZE)
 
         if done:
             # +1 to account for 0 indexing. +0 on ep_timesteps since it will increment +1 even if done=True
@@ -130,8 +113,8 @@ if __name__ == "__main__":
             episode_num += 1
 
         # Evaluate episode
-        if (t + 1) % args.eval_freq == 0:
-            evaluations.append(eval_policy(policy, args.env, args.seed))
+        if (t + 1) % config.EVAL_FREQ == 0:
+            evaluations.append(eval_policy(policy, config.ENV, config.SEED))
             np.save(f"./results/{file_name}", evaluations)
-            if args.save_model:
+            if config.SAVE_MODEL:
                 policy.save(f"./models/{file_name}")
